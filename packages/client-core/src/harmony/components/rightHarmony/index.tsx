b9accc7f7778cb04b5d8d6a87715d6cfc024fc55
kimenyi kevin <kimenyikevin@gmail.com>
2021-10-20 20:58:30 +0200

    implement ui invite

--- packages/client-core/src/harmony/components/rightHarmony/index.tsx
+++ packages/client-core/src/harmony/components/rightHarmony/index.tsx
@@ -47,8 +47,6 @@ export default function RightHarmony() {
     setOpenInvite(open)
   }
 
-  console.log('RRRRRRRRRR', activeChannel)
-
   useEffect(() => {
     if (channelState.updateNeeded) {
       ChatService.getChannels()
